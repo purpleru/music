d5c66877cf9944a75718ec27a1b8fe56df4d23e2
gudu <3072953029@qq.com>
2022-06-30 23:45:02 +0800

    调整server.js端口问题

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 
 const path = require('path');
 
+const port = 3006;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3006, () => {
-    console.log('服务启动成功,请访问http://127.0.0.1:3006');
+app.listen(port, () => {
+    console.log('服务启动成功,请访问http://127.0.0.1:' + port);
 });
